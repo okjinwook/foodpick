23a5b9332aef85b60972d2b0b966aeb61c39b7f3
cdfor815 <cdfor815@naver.com>
2023-03-25 21:18:04 +0900

    map api 현재위치 + 키워드

--- src/components/step/kakaoMap.js
+++ src/components/step/kakaoMap.js
@@ -6,20 +6,50 @@ const {kakao} = window;
 function KaKaoMap(){
     const foodKeyword = useSelector(store => store.foodSelectReducer)
     useEffect(() => {
+        // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
         var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
         var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-        mapOption = {
-            center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
-            level: 3 // 지도의 확대 레벨
-        };  
-    
+            mapOption = {
+                center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+                level: 3 // 지도의 확대 레벨
+            };  
+
         // 지도를 생성합니다    
         var map = new kakao.maps.Map(mapContainer, mapOption); 
 
         // 장소 검색 객체를 생성합니다
         var ps = new kakao.maps.services.Places(); 
 
-        ps.keywordSearch(foodKeyword + ' 맛집', placesSearchCB); 
+        const getCurrentCoordinate = async () => {
+            return new Promise((res, rej) => {
+              // HTML5의 geolocaiton으로 사용할 수 있는지 확인합니다.
+              if (navigator.geolocation) {
+                // GeoLocation을 이용해서 접속 위치를 얻어옵니다.
+                navigator.geolocation.getCurrentPosition(function (position) {
+                  const lat = position.coords.latitude; // 위도
+                  const lon = position.coords.longitude; // 경도
+          
+                  const coordinate = new kakao.maps.LatLng(lat, lon);
+                  res(coordinate);
+                });
+              } else {
+                rej(new Error("현재 위치를 불러올 수 없습니다."));
+              }
+            });
+        };
+        async function searchPlaces() {
+            const currentCoordinate = await getCurrentCoordinate();
+            var options = {
+              location: currentCoordinate,
+              radius: 20000,
+              sort: kakao.maps.services.SortBy.DISTANCE,
+            };
+            
+            // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
+            ps.keywordSearch(foodKeyword, placesSearchCB, options);
+          }
+          searchPlaces()
 
         // 키워드 검색 완료 시 호출되는 콜백함수 입니다
         function placesSearchCB (data, status, pagination) {
@@ -37,30 +67,6 @@ function KaKaoMap(){
                 // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
                 map.setBounds(bounds);
             } 
-        }
-        // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-        if (navigator.geolocation) {
-            
-            // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-            navigator.geolocation.getCurrentPosition(function(position) {
-                
-                var lat = position.coords.latitude, // 위도
-                    lon = position.coords.longitude; // 경도
-                
-                var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-                    message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
-                
-                // 마커와 인포윈도우를 표시합니다
-                displayMarker(locPosition, message);
-                    
-            });
-            
-        } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-            
-            var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
-                message = 'geolocation을 사용할수 없어요..'
-                
-            displayMarker(locPosition, message);
         }
 
         // 지도에 마커를 표시하는 함수입니다
